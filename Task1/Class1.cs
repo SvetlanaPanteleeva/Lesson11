87b96fdbfa97bfd5958800cec675d8ae6b9c7e29
Светлана Пантелеева <panteleevas@gmail.com>
2023-02-03 23:07:56 +0300

    Добавьте файлы проекта.

---
+++ Task1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct Linearequation
+    {
+        double b;
+        double k;
+
+        public Linearequation(double k, double b)
+        {
+            this.b = b;
+            this.k = k;
+        }
+        public string Root()
+        {
+            double x = -b / k;
+            if (k == 0)
+                return "Деление на 0 невозможно";
+            else
+                return $"x = {x}";
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b = Convert.ToDouble(Console.ReadLine());
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            Linearequation linearequation = new Linearequation(k, b);
+            Console.WriteLine(linearequation.Root());
+            Console.ReadKey();
+        }
+    }
+}
